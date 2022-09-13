ef6c153a0e7ed38539ec7dbebdeb6b365344b090
daniel <dwinney@iu.edu>
2022-09-13 13:49:05 +0800

    added DsDpi final state with D-wave D1 transition

--- include/core/DsDpi_decay.hpp
+++ include/core/DsDpi_decay.hpp
@@ -4,7 +4,9 @@
 // Email:        dwinney@scnu.edu.cn
 // ---------------------------------------------------------------------------
 
+#include "constants.hpp"
 #include "decay_process.hpp"
+#include "propagator.hpp"
 
 #ifndef DSDPI
 #define DSDPI
@@ -19,16 +21,39 @@ class DsDpi_decay : public decay_process
     // b - D
     // c - pi
     DsDpi_decay() 
-    : decay_process(_masses, _labels, 0, "DsDpi")
+    : decay_process(_masses, _labels, 0, "DsDpi"),
+      D1_prop(M_D1, GAM_D1)
     {};
 
 
     // Output the sum of diagrams
-    inline double amplitude_squared(double s, double sab, double sbc){ return 1.; };
+    inline double amplitude_squared(double s, double sab, double sbc)
+    {
+        update(s, sab, sbc);
+        return dwave_tree_squared(); 
+    };
+
     protected:
+
+    // ------------------------------------------------------------------------
+    // D-wave contributions
+    
+    nonrelativistic_BW D1_prop;
+
+    double dwave_tree_squared();
+
+    // ------------------------------------------------------------------------
+    // S-wave contributions
+
+    // Output the modulus of pion momentum in the Y rest frame
+    inline double pion_momentum()       { return sqrt(Kallen(_s, _sab, _mc2) / (4. * _s)); };
+    inline double pion_momentum_squared(){ return      Kallen(_s, _sab, _mc2) / (4. * _s); };
 
     static const array<string,3> _labels;
     static const array<double,3> _masses;
+
+    // Constants specific to this amplitude
+    static const double GAM_D1; // (charged) D1 width in GeV
 };
 
 #endif
