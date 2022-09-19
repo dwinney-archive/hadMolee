c1b4897c639da67688531399ab6ac28bab0201b0
daniel <dwinney@iu.edu>
2022-09-19 16:42:56 +0800

    added more nice little plotting scripts

--- scripts/triangle_singularities.cpp
+++ scripts/triangle_singularities.cpp
@@ -5,10 +5,10 @@
 
 using namespace std;
 
-void triangles_plot()
+void triangle_singularities()
 {   
-    array<double,3> internal = {M_DSTAR, M_D1,  M_D};
-    array<double,3> external = {M_Y,     M_Z,   M_PION};
+    array<double,3> internal = {M_DSTAR, M_D1,     M_D};
+    array<double,3> external = {M_Y4260, M_ZC3900, M_PION};
 
     relativistic_triangle r_triangle;
     r_triangle.set_internal_masses(internal);
@@ -26,28 +26,18 @@ void triangles_plot()
     int N = 100;
     bool PRINT = true;
 
-    // double xmin = 4.2;
-    // double xmax = 4.4;
-    // double ymin = -1.;
-    // double ymax = 5.;
-    // string xlabel   = "#sqrt{#it{s}}  [GeV]";
-    
-    double xmin = 3.8;
-    double xmax = 4.1;
-    double ymin = 0.;
-    double ymax = 1.5;
-    string xlabel   = "#sqrt{#sigma_{#it{D}*#it{D}}}  [GeV]";
+    double xmin, xmax, ymin, ymax;
+    string xlabel, filename;
 
     string ylabel   = "#it{T}_{#it{D}*#it{D}_{1}#it{D}} #times 100";
-    string filename = "tri_test.pdf";
 
     // Plotter object
-    jpacGraph1D * plotter = new jpacGraph1D();
+    jpacGraph1D * plotter = new jpacGraph1D(0);
 
     int i;
     auto re = [&](double E)
     {
-        external[1] = E;
+        external[0] = E;
         triangles[i]->set_external_masses(external);
         return real(triangles[i]->eval() * 1.E2);
     };
@@ -57,6 +47,13 @@ void triangles_plot()
         triangles[i]->set_external_masses(external);
         return imag(triangles[i]->eval() * 1.E2);
     };
+
+    xmin = 4.2;
+    xmax = 4.4;
+    ymin = -1.;
+    ymax = 5.;
+    xlabel   = "#sqrt{#it{s}}  [GeV]";
+    filename = "triangle_s.pdf";
 
     i = 0;
     plotter->AddEntry(N, re, {xmin, xmax}, "Real", PRINT);
@@ -72,7 +69,45 @@ void triangles_plot()
     plotter->SetYaxis(ylabel, ymin, ymax);
     plotter->SetLegend(0.7,0.7);
     plotter->SetLegendOffset(0.4, 0.07);
-    plotter->RemoveLogo();
+    plotter->Plot(filename);
+
+    plotter->ClearData();
+    external = {M_Y4260, M_ZC3900, M_PION};
+
+    auto re2 = [&](double E)
+    {
+        external[1] = E;
+        triangles[i]->set_external_masses(external);
+        return real(triangles[i]->eval() * 1.E2);
+    };
+    auto im2 = [&](double E)
+    {
+        external[1] = E;
+        triangles[i]->set_external_masses(external);
+        return imag(triangles[i]->eval() * 1.E2);
+    };
+
+    xmin = 3.8;
+    xmax = 4.1;
+    ymin = 0.;
+    ymax = 1.5;
+    xlabel   = "#sqrt{#sigma_{#it{D}*#it{D}}}  [GeV]";
+    filename = "triangle_sab.pdf";
+
+    i = 0;
+    plotter->AddEntry(N, re2, {xmin, xmax}, "Real", PRINT);
+    i = 1;
+    plotter->AddDashedEntry(N, re2, {xmin, xmax}, PRINT);
+
+    i = 0;
+    plotter->AddEntry(N, im2, {xmin, xmax}, "Imaginary", PRINT);
+    i = 1;
+    plotter->AddDashedEntry(N, im2, {xmin, xmax}, PRINT);
+
+    plotter->SetXaxis(xlabel, xmin, xmax);
+    plotter->SetYaxis(ylabel, ymin, ymax);
+    plotter->SetLegend(0.7,0.7);
+    plotter->SetLegendOffset(0.4, 0.07);
     plotter->Plot(filename);
 
     delete plotter;
