20fc8fd4baca44c8ba1bd92043977ba8dc777d4d
dwinney <daniel.winney@gmail.com>
2023-11-28 14:31:02 +0100

    boxes look closer

--- physics/Jpsipipi/boxes.hpp
+++ physics/Jpsipipi/boxes.hpp
@@ -52,15 +52,14 @@ namespace hadMolee::Jpsipipi
         // D1 -> D* pi coupling
         inline complex D1_coupling(cartesian_index i, cartesian_index j)
         {
-            return sqrt(M_D1*M_DSTAR) * (H1_D*(3.*p1(i)*p1(j) - delta(i,j)*modp()*modp()) + H1_S*delta(i,j));
+            double swave = H1_S*sqrt(modp1()*modp1() + M_PION*M_PION) * delta(i,j);
+            double dwave = H1_D*(3.*p1(i)*p1(j) - delta(i,j)*modp1()*modp1());
+
+            return sqrt(M_D10*M_DSTAR0)*(swave + dwave);
         };
 
         inline void recalculate()
         {
-            // Y mass and couplings
-            double gy   = _Y->coupling();
-            double M_Y = sqrt(_s);
-
             // Update floating masses in the box and evaluate
             _B.set_external_masses({_W,   _decay_masses[0], _decay_masses[1], _decay_masses[2]});
             _B.set_internal_masses({M_D1, _loop_masses[0],  _loop_masses[1],  _loop_masses[2] });
@@ -73,12 +72,12 @@ namespace hadMolee::Jpsipipi
             _q_dot_p[0] =  q(x)*p2(x) + q(y)*p2(y) + q(z)*p2(z);
             
             _pi1 = particle::b;
-            _B.set_invariant_masses(permute(_schan),   permute(_tchan));
+            _B.set_invariant_masses(permute(_schan), permute(_tchan));
             _vB[1] = _B.eval_vector();
             _q_dot_p[1] =  q(x)*p2(x) + q(y)*p2(y) + q(z)*p2(z);
         
             // go around the box accumulating the common couplings
-            _C  = gy / sqrt(2.) * sqrt(M_Y*M_D1*M_D); 
+            _C  = _Y->coupling()/sqrt(2.) * sqrt(_Y->mass()*M_D1*M_D0); 
             // Skip the D1 -> D* pi coupling which we include above
             _C *= _gjpsi;
             _C *= _gpi;
@@ -110,9 +109,10 @@ namespace hadMolee::Jpsipipi
         };
 
         // Aliases for the pion momenta specifying which is pi1 and pi2
-        inline double modp(){ return (_pi1 == c) ? _mpc : _mpb; };
-        inline complex p1(cartesian_index i){ return modp() * phat_1(i); };
-        inline complex p2(cartesian_index i){ return modp() * phat_2(i); };
+        inline double modp1(){ return (_pi1 == c) ? _mpc : _mpb; };
+        inline double modp2(){ return (_pi1 == c) ? _mpb : _mpc; };
+        inline double p1(cartesian_index i){ return modp1() * phat_1(i); };
+        inline double p2(cartesian_index i){ return modp2() * phat_2(i); };
 
         // Vector decomposition of the Box
         inline std::array<complex,4> vB(){ return (_pi1 == particle::c) ? _vB[0] : _vB[1]; };
@@ -127,14 +127,11 @@ namespace hadMolee::Jpsipipi
         virtual inline complex M(cartesian_index j, cartesian_index k) = 0;
 
         // Scalar prefactors 
-        double _C     = 0.;
-        double _gjpsi = 0., _gpi = 0.; 
+        double _C = 0., _gjpsi = 0., _gpi = 0.; 
 
-        std::array<double,3> _decay_masses;
-        std::array<double,3> _loop_masses;
+        std::array<double,3> _decay_masses, _loop_masses;
 
         // Box functions
-        // Fixed masses for all the particles in the box
         box _B;
 
         // Array to store the vector components of the box integral
@@ -158,11 +155,11 @@ namespace hadMolee::Jpsipipi
         {
             _schan = ab, _tchan = ac;
 
-            _decay_masses = {M_PION, M_JPSI,  M_PION};
-            _loop_masses  = {M_D,    M_DSTAR, M_DSTAR};
+            _decay_masses = {M_PION,  M_JPSI,   M_PION};
+            _loop_masses  = {M_D0,    M_DSTAR0, M_DSTAR0};
 
-            _gjpsi =  G_PSI   * sqrt(M_JPSI*M_DSTAR*M_DSTAR);
-            _gpi   =  G1_PION * sqrt(M_DSTAR * M_D); 
+            _gjpsi =  2.*G_PSI * sqrt(M_JPSI*M_DSTAR0*M_DSTAR0);
+            _gpi   =  G1_PION; 
         };
 
         protected:
@@ -173,18 +170,14 @@ namespace hadMolee::Jpsipipi
         {
             auto vecB = vB();
 
-            // 2 l + (p2 - p1)
-            // = 2 (D2 p1 + D3 (p1 + p3)) + D0 (p1 - p2)
-            // with p3 = - (p1 + p2)
-            complex pi1_piece = - (vecB[0] + 2.*vecB[2]);
-            complex pi2_piece = + (vecB[0] + 2.*vecB[3]);
-
+            complex pi1_piece = + (vecB[0] + 2.*vecB[2]);
+            complex pi2_piece = - (vecB[0] + 2.*vecB[3]);
             return pi1_piece * p1(i) + pi2_piece * p2(i);
         };
 
         inline complex M(cartesian_index j, cartesian_index k)
         {
-            return q(k)*p2(j) - q(j)*p2(k) - delta(j,k)*qdotp2();
+            return p2(j)*q(k) - p2(k)*q(j) - delta(j, k)*qdotp2();
         };
     };
 
@@ -198,11 +191,11 @@ namespace hadMolee::Jpsipipi
         {
             _schan = bc, _tchan = ab;
 
-            _decay_masses = {M_JPSI, M_PION,  M_PION};
-            _loop_masses  = {M_D,    M_DSTAR, M_DSTAR};
+            _decay_masses = {M_JPSI,  M_PION,   M_PION};
+            _loop_masses  = {M_D0,    M_DSTAR0, M_DSTAR0};
 
-            _gjpsi = - G_PSI   * sqrt(M_JPSI*M_DSTAR*M_D); 
-            _gpi   = G2_PION * sqrt(M_DSTAR * M_DSTAR); 
+            _gjpsi = 2.*G_PSI * sqrt(M_JPSI*M_DSTAR0*M_D0); 
+            _gpi   = G2_PION; 
         };
 
         protected:
@@ -212,17 +205,14 @@ namespace hadMolee::Jpsipipi
         inline complex q(cartesian_index i)
         {
             auto vecB = vB();
-
-            // 2 l - (p2 + p1)
             complex pi1_piece = - (2.*vecB[3] + 2.*vecB[2] + vecB[0]);
             complex pi2_piece = - (2.*vecB[3]              + vecB[0]);
-
             return pi1_piece * p1(i) + pi2_piece * p2(i);
         };
 
         inline complex M(cartesian_index j, cartesian_index k)
         {
-            return q(j)*p2(k) - delta(j,k)*qdotp2();
+            return p2(k)*q(j) - delta(j, k)*qdotp2();
         };
     };
 
@@ -236,11 +226,11 @@ namespace hadMolee::Jpsipipi
         {
             _schan = bc, _tchan = ab;
 
-            _decay_masses = {M_JPSI, M_PION,  M_PION};
-            _loop_masses  = {M_D,    M_D,     M_DSTAR};
+            _decay_masses = {M_JPSI,  M_PION,   M_PION};
+            _loop_masses  = {M_D0,    M_D0,     M_DSTAR0};
 
-            _gjpsi = G_PSI   * sqrt(M_JPSI*M_D*M_D); 
-            _gpi   = G1_PION * sqrt(M_DSTAR * M_D); 
+            _gjpsi = 2.*G_PSI * sqrt(M_JPSI*M_D0*M_D0); 
+            _gpi   = G1_PION; 
         };
 
         protected:
@@ -250,17 +240,14 @@ namespace hadMolee::Jpsipipi
         inline complex q(cartesian_index i)
         {
             auto vecB = vB();
-
-            // 2 l - (p2 + p1)
             complex pi1_piece = - (2.*vecB[3] + 2.*vecB[2] + vecB[0]);
             complex pi2_piece = - (2.*vecB[3]              + vecB[0]);
-
             return pi1_piece * p1(i) + pi2_piece * p2(i);
         };
 
         inline complex M(cartesian_index j, cartesian_index k)
         {
-            return q(k)*p2(j);
+            return p2(j)*q(k);
         };
     };
 };
